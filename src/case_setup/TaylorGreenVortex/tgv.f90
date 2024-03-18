aa03fc6a9a7812dc3bd1b11489618e4006f00407
Shrutee Lakshminarasimha <shrutee.lakshminarasimha2@rolls-royce.com>
2024-03-18 14:31:47 +0000

    TGV3D: working fine (sol reader and writed in HDF5 format)

--- src/case_setup/TaylorGreenVortex/tgv.f90
+++ src/case_setup/TaylorGreenVortex/tgv.f90
@@ -16,12 +16,12 @@ program tgv
                        field_u, field_v, field_w, field_p, field_p_prime, field_mf, field_viscosity, &
                        field_density, cell_centred_central, cell_centred_upwind, face_centred
   use constants, only: ccs_split_type_shared, ccs_split_type_low_high, ccs_split_undefined
-  use meshing, only: set_mesh_object, nullify_mesh_object
+  use meshing, only: set_mesh_object, nullify_mesh_object, get_local_num_cells
   use fields, only: create_field, set_field_config_file, set_field_n_boundaries, set_field_name, &
                     set_field_type, set_field_vector_properties, set_field_store_residuals, set_field_enable_cell_corrections
   use fortran_yaml_c_interface, only: parse
   use fv, only: update_gradient
-  use io_visualisation, only: write_solution
+  use io_visualisation, only: write_solution, read_solution
   use kinds, only: ccs_real, ccs_int, ccs_long
   use mesh_utils, only: read_mesh, build_mesh, write_mesh
   use parallel, only: initialise_parallel_environment, &
@@ -43,7 +43,7 @@ program tgv
                    add_field_to_outputlist, get_field, add_field, &
                    get_fluid_solver_selector, set_fluid_solver_selector, &
                    allocate_fluid_fields, str, debug_print
-  use vec, only: create_vector, set_vector_location
+  use vec, only: create_vector, set_vector_location, get_vector_data, restore_vector_data
   use timers, only: timer_init, timer_register_start, timer_register, timer_start, timer_stop, timer_print, &
                     timer_get_time, timer_print_all, timer_export_csv
 
@@ -55,6 +55,10 @@ program tgv
   character(len=:), allocatable :: case_path  ! Path to input directory with case name appended
   character(len=:), allocatable :: ccs_config_file ! Config file for CCS
   character(len=ccs_string_len), dimension(:), allocatable :: variable_names  ! variable names for BC reading
+
+  real(ccs_real), dimension(:), pointer :: output_data
+  integer(ccs_int) :: index_p
+  integer(ccs_int) :: n_local
 
   type(vector_spec) :: vec_properties
 
@@ -243,6 +247,16 @@ program tgv
   call add_field(mf, flow_fields)
   call add_field(viscosity, flow_fields) 
   call add_field(density, flow_fields) 
+
+  call read_solution(par_env, case_path, mesh, output_list)
+  call get_vector_data(output_list(1)%ptr%values, output_data)
+
+  call get_local_num_cells(n_local)
+  do index_p = 1, n_local
+    print*, index_p, output_data(index_p)
+  end do
+
+  call restore_vector_data(output_list(1)%ptr%values, output_data)
 
   call timer_stop(timer_index_init)
   call timer_register("I/O time for solution", timer_index_io_sol)
