ce7f39da15df7484b397b55c0ede7eb6880f477a
Shrutee Lakshminarasimha <shrutee.lakshminarasimha2@rolls-royce.com>
2024-05-28 09:26:38 +0100

    SA: print statements removed

--- src/case_setup/ScalarAdvection/scalar_advection.f90
+++ src/case_setup/ScalarAdvection/scalar_advection.f90
@@ -226,17 +226,6 @@ program scalar_advection
   call finalise(M)
 
   call read_solution(par_env, case_path, mesh, flow_fields)
-  call get_field(flow_fields, "u", u) 
-  call get_vector_data(u%values, output_data)
-
-  call get_local_num_cells(n_local)
-  do index_p = 1, n_local
-    print*, index_p, output_data(index_p)
-  end do
-
-  call restore_vector_data(u%values, output_data)
-  call update(u%values)
-  nullify(u)
 
   ! Create linear solver & set options
   if (irank == par_env%root) print *, "Solve"
@@ -248,15 +237,6 @@ program scalar_advection
 
   call write_mesh(par_env, case_path, mesh)
   call write_solution(par_env, case_path, mesh, flow_fields)
-
-  call get_vector_data(u%values, output_data)
-  
-  call get_local_num_cells(n_local)
-  do index_p = 1, n_local
-    print*, index_p, output_data(index_p)
-  end do
-
-  call restore_vector_data(u%values, output_data)
 
   ! Clean up
   deallocate(source)
