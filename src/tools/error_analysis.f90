eb1a29cbf5469747a18c0242582b6e05402b45ad
Alexei <a.borissov@epcc.ed.ac.uk>
2024-06-16 21:13:26 +0100

    use setters and getters for accesing cell centres

--- src/tools/error_analysis.f90
+++ src/tools/error_analysis.f90
@@ -122,15 +122,20 @@ contains
     do icell = 1, total_num_cells
       icell_global = mesh%topo%global_indices(icell)
 
-      if (mesh%geo%x_p(1, icell) >= 0.1 * domain_size .and. mesh%geo%x_p(1, icell) <= 0.9 * domain_size .and. &
-          mesh%geo%x_p(2, icell) >= 0.1 * domain_size .and. mesh%geo%x_p(2, icell) <= 0.9 * domain_size) then
+      call create_cell_locator(icell, loc_p)
+      call get_centre(loc_p, x_p)
+
+      if (x_p(1) >= 0.1 * domain_size .and. x_p(1) <= 0.9 * domain_size .and. &
+          x_p(2) >= 0.1 * domain_size .and. x_p(2) <= 0.9 * domain_size) then
         idim = 1
         disturbance = real(modulo(3 * icell_global, 17), ccs_real) / 17.0_ccs_real
-        mesh%geo%x_p(idim, icell) = mesh%geo%x_p(idim, icell) + (disturbance - 0.5_ccs_real) * dx / 20.0
+        x_p(idim) = x_p(idim) + (disturbance - 0.5_ccs_real) * dx / 20.0
 
         idim = 2
         disturbance = real(modulo(3 * icell_global, 29), ccs_real) / 29.0_ccs_real
-        mesh%geo%x_p(idim, icell) = mesh%geo%x_p(idim, icell) + (disturbance - 0.5_ccs_real) * dx / 20.0
+        x_p(idim) = x_p(idim) + (disturbance - 0.5_ccs_real) * dx / 20.0
+
+        call set_centre(loc_p, x_p)
       end if
 
     end do
