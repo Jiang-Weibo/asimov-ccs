8963c29d826a10edcbce5238c78631353dbf91f3
Shrutee Lakshminarasimha <shrutee.lakshminarasimha2@rolls-royce.com>
2024-07-16 06:02:37 +0100

    Poiseuille: refining code

--- src/case_setup/Poiseuille/poiseuille_core.f90
+++ src/case_setup/Poiseuille/poiseuille_core.f90
@@ -253,45 +253,17 @@ module poiseuille_core
       call read_solution(par_env, case_path, mesh, flow_fields)
     end if 
 
-    if (unsteady) then
-      print*, "unsteady-state activated"
-      do t = 1, num_steps
-        call timer_register_start("Solver time inc I/O", timer_index_sol)
-        call solve_nonlinear(par_env, mesh, it_start, it_end, res_target, &
-                          flow_fields)
-
-        ! This could be a postprocessing subroutine
-        call get_field(flow_fields, "u", u)
-        call get_field(flow_fields, "v", v)
-        call get_field(flow_fields, "w", w)
-        call get_field(flow_fields, "p", p)
-        call calc_kinetic_energy(par_env, u, v, w)
-        call calc_enstrophy(par_env, u, v, w)
-
-        call calc_error(par_env, u, v, p, error_L2, error_Linf)
-        nullify(u)
-        nullify(v)
-        nullify(w)
-        nullify(p)
-        nullify(mf)
-
-        if (par_env%proc_id == par_env%root) then
-          print *, "TIME = ", t
-        end if
-
-        if ((t == 1) .or. (t == num_steps) .or. (mod(t, write_frequency) == 0)) then
-          call timer_start(timer_index_io_sol)
-          call write_solution(par_env, case_path, mesh, flow_fields, t, num_steps, dt)
-          call timer_stop(timer_index_io_sol)
-        end if
-
-        call timer_stop(timer_index_sol)
-      end do
-    else
+    if(.not.unsteady) then
+      num_steps = 1
       print*, "steady-state activated"
+    else
+      print*, "unsteady-state activated"
+    end if
+
+    do t = 1, num_steps
       call timer_register_start("Solver time inc I/O", timer_index_sol)
       call solve_nonlinear(par_env, mesh, it_start, it_end, res_target, &
-                          flow_fields)
+                        flow_fields)
 
       ! This could be a postprocessing subroutine
       call get_field(flow_fields, "u", u)
@@ -308,13 +280,22 @@ module poiseuille_core
       nullify(p)
       nullify(mf)
 
-      call write_solution(par_env, case_path, mesh, flow_fields)
+      if (par_env%proc_id == par_env%root) then
+        print *, "TIME = ", t
+      end if
+
+      if ((t == 1) .or. (t == num_steps) .or. (mod(t, write_frequency) == 0)) then
+        if(.not. unsteady) then
+          call write_solution(par_env, case_path, mesh, flow_fields)
+        else
+          call timer_start(timer_index_io_sol)
+          call write_solution(par_env, case_path, mesh, flow_fields, t, num_steps, dt)
+          call timer_stop(timer_index_io_sol)
+        end if
+      end if
 
       call timer_stop(timer_index_sol)
-
-    end if 
-
-    
+    end do 
     
     ! Clean-up
 
